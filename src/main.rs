98b14ceab30b95c1c3dd5cfc02f551bbf3d79e6b
houssam lamlih <houssam.lamlih@hotmail.com>
2023-07-28 13:17:56 +0100

    Initial grammar + simple parsing using pest

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use pest_derive::*;
+use pest::*;
+use std::fs;
+
+
+#[derive(Parser)]
+#[grammar = "grammar.pest"]
+struct MdParser;
+
+fn main() {
+    let input = fs::read_to_string("sample.md").expect("Failed to read the file.");
+
+    match MdParser::parse(Rule::start, &input) {
+        Ok(parsed) => {
+            for pair in parsed {
+                println!("Rule: {:#?}, Span: {:#?}", pair.as_rule(), pair.as_span());
+            }
+        }
+        Err(e) => eprintln!("Error while parsing: {:?}", e),
+    }    
+}
