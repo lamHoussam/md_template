3783905d5412aa4526c9aefcefd3481caf9ac547
houssam lamlih <houssam.lamlih@hotmail.com>
2023-09-25 18:18:17 +0100

    Make data_file argument optional

--- src/main.rs
+++ src/main.rs
@@ -16,13 +16,13 @@ pub use symbol::{Symbol, get_symbol_from_variable_value};
 #[derive(ClapParser, Debug)]
 #[command()]
 struct Args {
-    /// Name of the person to greet
+    /// Data file path for global variables
     #[arg(short, long)]
-    data: String,
-
+    data: Option<String>,
+    // Sample markdown file
     #[arg(short, long)]
     sample: String,
-
+    // Output markdown file
     #[arg(short, long)]
     output: String,
 }
@@ -32,38 +32,39 @@ fn main() {
     
     let args = Args::parse();
     
-    println!("Data: {}, Sample: {}, Output: {}", args.data, args.sample, args.output);
+    println!("Sample: {}, Output: {}", args.sample, args.output);
     
-    let data_file_path: &str = args.data.as_str();
     let sample_file_path: &str = args.sample.as_str();
     let output_file_path: &str = args.output.as_str();
 
-    let data_file: String = fs::read_to_string(data_file_path).expect("Failed to read data file");
     let sample_file: String = fs::read_to_string(sample_file_path).expect("Failed to read sample file.");
     
     let mut output_file: File = match File::create(output_file_path) {
         Ok(file) => file,
         Err(_) => panic!("Couldn't create output file"),
     };
-
+    
     let mut global_variables: HashMap<String, Symbol> = HashMap::new();
     let mut local_variables: HashMap<String, Symbol> = HashMap::new();
-
+    
     // println!("Data file: ");
-
-    match MdParser::parse(Rule::start, &data_file) {
-        Ok(parsed) => {
-            match MdParser::parse_syntax_tree(&parsed, &mut global_variables, &mut local_variables) {
-                Ok(_) => {},
+    match args.data {
+        Some(data_file_path) => {
+            let data_file: String = fs::read_to_string(data_file_path).expect("Failed to read data file");
+        
+            match MdParser::parse(Rule::start, &data_file) {
+                Ok(parsed) => {
+                    match MdParser::parse_syntax_tree(&parsed, &mut global_variables, &mut local_variables) {
+                        Ok(_) => {},
+                        Err(e) => eprintln!("Error while parsing: {:?}", e),
+                    };
+                }
                 Err(e) => eprintln!("Error while parsing: {:?}", e),
-            };
-        }
-        Err(e) => eprintln!("Error while parsing: {:?}", e),
-    }
+            }
+        },    
+        None => {},
+    }    
 
-    // println!("Variables: {:#?}", global_variables);
-
-    // println!("Sample file: ");
     match MdParser::parse(Rule::start, &sample_file) {
         Ok(parsed) => {
             println!("Output {:#?}", parsed);
