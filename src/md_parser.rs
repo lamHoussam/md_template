e090198c6569f49dc5595a0f7c5fd9a0947811c5
houssam lamlih <houssam.lamlih@hotmail.com>
2024-02-05 19:33:21 -0500

    Rewrite parse functions

--- src/md_parser.rs
+++ src/md_parser.rs
@@ -64,9 +64,109 @@ impl<'a> MdParser<'a> {
             _ => Expr::None,
         }
     }
-
-    fn parse_assignment(&mut self) {
+    
+    fn parse_assignment(&mut self, identifier: String) -> Statement {
+        let op = self.tokens.pop_front().expect("Need operator here");
+        // Handle Syntax Error
+        if op.token_type != MdTokenType::Assign {
+            return Statement::None;
+        }
+    
+        // TODO: Implement different data assign
+        let mut left: String = String::new();
+        let frst = self.tokens.pop_front().expect("Need token here");
+        match frst.token_type {
+            MdTokenType::String(val) => left.push_str(&val),
+            _ => println!("Need string here"),
+        }
+        loop {
+            let tk = self.tokens.pop_front().expect("Incomplete assignment");
+            match tk.token_type { 
+                MdTokenType::Operator('.') => {
+                    let nxt = self.tokens.pop_front().expect("Need right part for string concat");
+                    match nxt.token_type {
+                        MdTokenType::String(val) => left.push_str(&val),
+                        _ => println!("Cant concatenate string with other"),
+                    }
+                },
+                MdTokenType::EndStatement => {
+                    return Statement::Assignment(Expr::Identifier(identifier), Expr::Litteral(Symbol::from(left)));
+                },
+                // Handle Error
+                _ => break,
+            }                        
+        }
+    
+        return Statement::None;
+    }
+    
+    fn parse_if_statement(&mut self) -> Statement {
+        let mut condition: Vec<MdToken> = Vec::new();
+        loop {
+            let tk = self.tokens.pop_front().expect("Error");
+            if tk.token_type == MdTokenType::Then {
+                break;
+            }
+            condition.push(tk);
+        }
+        // For now we only take one token as condition, later need to implement Operations
+        let cond = condition.first().expect("Error");
+        let mut statements: Vec<Statement> = Vec::new();
+        match cond.token_type {
+            MdTokenType::Dereference => {
+                // TODO: Evaluate dereferenced value
+            },
+            MdTokenType::True => {
+                loop {
+                    let sttment: Statement = self.parse_statement();
+                    if sttment == Statement::End(MdTokenType::Else) {
+                        break;
+                    }
+                    else if sttment == Statement::End(MdTokenType::Endif) {
+                        return Statement::IfStatement(statements);
+                    }
+                    statements.push(sttment);
+                }
+                let mut if_count = 0;
+                loop {
+                    let tk = self.tokens.pop_front().expect("msg");
+                    if tk.token_type == MdTokenType::If { if_count += 1; }
+                    // println!("Token: {:?}", tk);
+                    if tk.token_type == MdTokenType::Endif {
+                        if if_count == 0 { break; }
+                        if_count -= 1;
+                    }
+                }
+                println!("Got if statement");
+                return Statement::IfStatement(statements);
+            },
+            MdTokenType::False => {
+                loop {
+                    let tk = self.tokens.pop_front().expect("msg");
+                    if tk.token_type == MdTokenType::Else || tk.token_type == MdTokenType::Endif {
+                        break;
+                    }
+                }
+                return Statement::IfStatement(vec![]);
+            },
+            _ => {
+                println!("Handle error");
+                return Statement::None;
+            }
+        }                    
         
+        return Statement::None;
+    }
+    
+    fn parse_for_statement(&mut self) -> Statement {
+        return Statement::None;
+
+    }
+
+    fn parse_print_statement(&mut self) -> Statement {
+        let tk = self.tokens.pop_front().expect("msg");
+        // println!("Print token: {:?}", tk.token_type);
+        return Statement::PrintStatement(Expr::Identifier(tk.lexem));
     }
     
     fn parse_statement(&mut self) -> Statement {
@@ -74,103 +174,20 @@ impl<'a> MdParser<'a> {
             match token.token_type {
                 MdTokenType::EndOfFile => { return Statement::End(token.token_type) },
                 MdTokenType::If => {
-                    let mut condition: Vec<MdToken> = Vec::new();
-                    loop {
-                        let tk = self.tokens.pop_front().expect("Error");
-                        if tk.token_type == MdTokenType::Then {
-                            break;
-                        }
-                        condition.push(tk);
-                    }
-                    // For now we only take one token as condition, later need to implement Operations
-                    let cond = condition.first().expect("Error");
-                    let mut statements: Vec<Statement> = Vec::new();
-                    match cond.token_type {
-                        MdTokenType::Dereference => {
-                            // TODO: Evaluate dereferenced value
-                        },
-                        MdTokenType::True => {
-                            loop {
-                                let sttment: Statement = self.parse_statement();
-                                if sttment == Statement::End(MdTokenType::Else) {
-                                    break;
-                                }
-                                else if sttment == Statement::End(MdTokenType::Endif) {
-                                    return Statement::IfStatement(statements);
-                                }
-                                statements.push(sttment);
-                            }
-                            let mut if_count = 0;
-                            loop {
-                                let tk = self.tokens.pop_front().expect("msg");
-                                if tk.token_type == MdTokenType::If { if_count += 1; }
-                                // println!("Token: {:?}", tk);
-                                if tk.token_type == MdTokenType::Endif {
-                                    if if_count == 0 { break; }
-                                    if_count -= 1;
-                                }
-                            }
-                            println!("Got if statement");
-                            return Statement::IfStatement(statements);
-                        },
-                        MdTokenType::False => {
-                            loop {
-                                let tk = self.tokens.pop_front().expect("msg");
-                                if tk.token_type == MdTokenType::Else || tk.token_type == MdTokenType::Endif {
-                                    break;
-                                }
-                            }
-                            return Statement::IfStatement(vec![]);
-                        },
-                        _ => {
-                            println!("Handle error");
-                            return Statement::None;
-                        }
-                    }                    
-
-                    return Statement::None;
+                    return self.parse_if_statement();
                 },
                 MdTokenType::Print => {
-                    let tk = self.tokens.pop_front().expect("msg");
-                    println!("Print token: {:?}", tk.token_type);
-                    return Statement::PrintStatement(Expr::Identifier(tk.lexem));
+                    return self.parse_print_statement();
+                },
+                MdTokenType::For => {
+                    // TODO: Parse for statement
+                    return Statement::None;
                 }
                 MdTokenType::Endfor | MdTokenType::Endif | MdTokenType::Else => {
                     return Statement::End(token.token_type);
                 },
                 MdTokenType::Identifier(identifier) => {
-                    let op = self.tokens.pop_front().expect("Need operator here");
-                    // Handle Syntax Error
-                    if op.token_type != MdTokenType::Assign {
-                        return Statement::None;
-                    }
-
-                    // TODO: Implement different data assign
-                    let mut left: String = String::new();
-                    let frst = self.tokens.pop_front().expect("Need token here");
-                    match frst.token_type {
-                        MdTokenType::String(val) => left.push_str(&val),
-                        _ => println!("Need string here"),
-                    }
-                    loop {
-                        let tk = self.tokens.pop_front().expect("Incomplete assignment");
-                        match tk.token_type { 
-                            MdTokenType::Operator('.') => {
-                                let nxt = self.tokens.pop_front().expect("Need right part for string concat");
-                                match nxt.token_type {
-                                    MdTokenType::String(val) => left.push_str(&val),
-                                    _ => println!("Cant concatenate string with other"),
-                                }
-                            },
-                            MdTokenType::EndStatement => {
-                                return Statement::Assignment(Expr::Identifier(identifier), Expr::Litteral(Symbol::from(left)));
-                            },
-                            // Handle Error
-                            _ => break,
-                        }                        
-                    }
-
-                    return Statement::None;
+                    return self.parse_assignment(identifier);
                 },
                 _ => {
                     // println!("Problems: {:?}", token);
